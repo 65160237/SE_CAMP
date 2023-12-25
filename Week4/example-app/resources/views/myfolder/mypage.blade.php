43cfb2c06bc41899360d8fd7804d2c378f3cfbd1
66160035 <66160035@go.buu.ac.th>
2023-12-25 19:54:36 +0700

    save

---
+++ Week4/example-app/resources/views/myfolder/mypage.blade.php
@@ -0,0 +1,18 @@
+<center>
+    <h1>My Floder and My Page </h1>
+<h1>
+    <?php echo $val_a. ' ' . $val_b; ?>
+</h1>
+<h1>{{$val_a}} {{$val_b}}</h1>
+<form method="post" action="/my-route">
+    @csrf
+    <input type="text" name ="myinput">
+    <button type="submit">Submit</button>
+</form>
+</center>
+<style>
+    body {
+ background-image: url("paper.gif");
+ background-color: #cccccc;
+}
+</style>
